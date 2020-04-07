3f4b6763974338bfacf09a944297978419aa5eb6
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-07 22:08:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ context.fillStyle = 'orange';
 context.fillRect(125,50,200,200);
 
 context.beginPath();
-context.strokeStyle = 'orange';
-context.moveTo(55,70);
-context.lineTo(150,100);
-context.stroke();
-
-context.beginPath();
 context.strokeStyle = 'red';
 context.moveTo(0,300);
 context.lineTo(450,0);
 context.stroke();
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+context.fillStyle = 'red';
+context.fillRect(200,150,canvasWidth-200,canvasHeight-150);
